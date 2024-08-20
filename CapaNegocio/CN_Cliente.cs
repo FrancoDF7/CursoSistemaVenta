ba8ebe0f2574cda5033b3624308c1ec579413aea
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2024-08-19 22:52:45 -0300

    Se codifico clase CN_Cliente

---
+++ CapaNegocio/CN_Cliente.cs
@@ -0,0 +1,83 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Cliente
+    {
+        //Instancia de la clase CD_Cliente de la CapaDatos
+        private CD_Cliente objcd_Cliente = new CD_Cliente();
+
+        //Retorna la lista obtenida del metodo Listar de la clase CD_Cliente 
+        public List<Cliente> Listar()
+        {
+            return objcd_Cliente.Listar();
+        }
+
+        public int Registrar(Cliente obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Cliente\n";
+            }
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje += "Es necesario el nombre completo del Cliente\n";
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesario la correo de Cliente\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objcd_Cliente.Registrar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Editar(Cliente obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Cliente\n";
+            }
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje += "Es necesario el nombre completo del Cliente\n";
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje += "Es necesario la correo de Cliente\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objcd_Cliente.Editar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Eliminar(Cliente obj, out string Mensaje)
+        {
+            return objcd_Cliente.Eliminar(obj, out Mensaje);
+        }
+    }
+}
