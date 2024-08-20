a5c671a3b662b353d216611685eee069b7cfede3
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2024-08-19 21:46:25 -0300

    Se codifico la clase CD_Cliente

---
+++ CapaDatos/CD_Cliente.cs
@@ -0,0 +1,178 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Cliente
+    {
+        //Este metodo devuelve una lista de Clientes.
+        public List<Cliente> Listar()
+        {
+            List<Cliente> lista = new List<Cliente>();
+
+            //Le pasa por parametros la cadena de conexion de la clase Conexion
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    //Consulta que se va realizar a la BD
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("SELECT IdCliente, Documento, NombreCompleto, Correo, Telefono, Estado FROM CLIENTE");
+
+                    //Le pasa por parametros al SqlCommand el la Consulta y Cadena de Conexion
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    //SqlDataReader se encarga de leer el resultado del SqlCommand
+                    //La variable de dr almacena el resultado del metodo ExecuteReader
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        //El bucle while(dr.Read()) se ejecuta mientras hay más filas de resultados para leer
+                        while (dr.Read())
+                        {
+                            //Se crea una nueva instancia de la clase Cliente.
+                            //Se asignan valores a las propiedades de Cliente utilizando los datos leídos de la fila actual
+                            //Después de asignar los valores a las propiedades de Cliente, se agrega la instancia de Cliente a una lista llamada lista
+                            lista.Add(new Cliente()
+                            {
+                                IdCliente = Convert.ToInt32(dr["IdCliente"]),
+                                Documento = dr["Documento"].ToString(),
+                                NombreCompleto = dr["NombreCompleto"].ToString(),
+                                Correo = dr["Correo"].ToString(),
+                                Telefono = dr["Telefono"].ToString(),
+                                Estado = Convert.ToBoolean(dr["Estado"]),                                
+                            });
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    //Si hay un error deja la lista vacia
+                    lista = new List<Cliente>();
+                }
+            }
+
+            return lista;
+        }
+
+
+        public int Registrar(Cliente obj, out string Mensaje)
+        {
+            int idClientegenerado = 0;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_RegistrarCliente", oconexion);
+                    cmd.Parameters.AddWithValue("Documento", obj.Documento);
+                    cmd.Parameters.AddWithValue("NombreCompleto", obj.NombreCompleto);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
+                    //Parametros de Salida
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; //Se debe pasar por parametros el tamaño del tipo de dato de sql, en este caso varchar(500)
+
+                    oconexion.Open();
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+                    idClientegenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idClientegenerado = 0;
+                Mensaje = ex.Message;
+            }
+
+
+            return idClientegenerado;
+        }
+
+
+        public bool Editar(Cliente obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EditarCliente", oconexion);
+                    cmd.Parameters.AddWithValue("IdCliente", obj.IdCliente);
+                    cmd.Parameters.AddWithValue("Documento", obj.Documento);
+                    cmd.Parameters.AddWithValue("NombreCompleto", obj.NombreCompleto);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    oconexion.Open();
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+
+            return respuesta;
+        }
+
+        public bool Eliminar(Cliente obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("DELETE FROM CLIENTE WHERE IdCliente = @id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", obj.IdCliente);
+
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    //La instruccion ExecuteNonQuery devuelve un numero de filas afectadas
+                    respuesta = cmd.ExecuteNonQuery() > 0 ? true : false; //Si el numero de filas afectadas es mayor a 0 (lo que quiere decir que elimino un cliente) el operador ternario devuelve TRUE
+
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+
+            return respuesta;
+        }
+
+    }
+}
